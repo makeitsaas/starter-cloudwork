0f2197ba68dc7391924ecac2dea0a25e0cfba521
Duwab <contact@superpaas.pro>
2019-03-07 12:35:44 +0100

    Mock database commands

--- cli.js
+++ cli.js
@@ -11,7 +11,13 @@ program
   .option('-D, --deploy', 'Deploy')
   .option('-S, --service [vars_file]', 'Generate config')
   .option('-P, --proxy [env]', 'Generate config')
+  .option('--database [action] [code]', 'Generate config')
+  .option('--app-code [code]', 'Generate config')
+  .option('--app-iteration [iteration]', 'Generate config')
   .parse(process.argv);
+
+const appCode = program.appCode,
+      appIteration = program.appCode;
 
 if(program.service) {
   console.log('generate config file for service', program.service);
@@ -22,6 +28,29 @@ if(program.proxy) {
 }
 
 if(program.deploy) {
-  console.log('launch deploy');
   require('./lib/deploy')
+}
+
+if(program.database) {
+  if(!appCode) {
+    return console.log('required arguments : --app-code');
+  }
+  console.log(program);
+  const db_name = `auto-db-${appCode}`,
+        db_user = `auto-user-${appCode}`;
+  switch(program.database) {
+    case "create":
+      console.log(`create db ${db_name} with user ${db_user}`);
+      break;
+    case "drop":
+      console.log(`drop db ${db_name} and user ${db_user}`);
+      break;
+    case "point-in-time":
+    case "dump":
+      console.log('not implemented yet');
+      break;
+    default:
+      console.log('unknown method');
+      break;
+  }
 }
